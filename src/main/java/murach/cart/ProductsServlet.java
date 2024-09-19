b1d56d2fde70c35abc8f5762f4b070fbae91d24e
Quang Vũ <vuxuanquang04@gmail.com>
2024-09-19 18:33:14 +0700

    First Commit

---
+++ src/main/java/murach/cart/ProductsServlet.java
@@ -0,0 +1,30 @@
+package murach.cart;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.*;
+import java.util.ArrayList;
+
+import murach.data.ProductIO;
+import murach.business.Product;
+
+public class ProductsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        String path = getServletContext().getRealPath("/WEB-INF/products.txt");
+        ArrayList<Product> products = ProductIO.getProducts(path);
+        session.setAttribute("products", products);
+
+        String url = "/index.jsp";
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+    }
+}
